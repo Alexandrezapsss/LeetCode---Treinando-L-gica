766c3f7edbcee9a0a7d9b1a7d2fc6c5cdd73ef70
Alexandrezapsss <alexandrequeirozdasilva94@gmail.com>
2024-04-02 22:35:07 -0300

    inicio da solução logica de palavras isometricas

---
+++ LeetCode - Javascript/palavrasIsometricas.js
@@ -0,0 +1,27 @@
+//teste de palavras isometricas ou seja que tenha grandesas iguais ex true : Ana, Ava - ex false: Faca,  Casa
+//teste de complexidade fácil porem tem que saber que palavras isometricas tem o mesmo peso
+
+let palavrasIsometricas = (n1, n2) =>{
+    //primeiro vamos padronizar os dados - vamos deixar elas minusculas
+    let palavra1 = n1.toLowerCase();
+    let palavra2 = n2.toLowerCase();
+
+    //segundo passo garantir que as palavras tenham o mesmo tamanho
+    if(palavra1 !== palavra2){
+        return false; // se as palavras não tiverm o mesmo tamanho já retornamos false
+    }
+    // vamos verificar a correspondencia - tem uma solução onde criamos um objeto e colocamos todas as letras mas não entendi a solução
+    // vou criar um obj para armazenar as correpondencias
+    let armazena = {}
+
+    //vou usar um loop para armazenar os caracteres das palavras 
+    for(let i = 0; palavra1.length; i++){
+        let carac1 = palavra1[i];
+        let carac2 = palavra2[i];
+
+        //fazer uma varificação com a palavra 2
+        if(armazena[carac1] && armazena[carac1] !== carac2){
+            return false;
+        }
+    }
+}
