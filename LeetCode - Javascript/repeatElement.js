3bda8732f193965ba98c956a53810f295909a2b0
Alexandrezapsss <alexandrequeirozdasilva94@gmail.com>
2024-04-08 22:26:16 -0300

    Aqui corrigi a questão se caso não for um numero e o que o usuario vai saber disso

--- LeetCode - Javascript/repeatElement.js
+++ LeetCode - Javascript/repeatElement.js
@@ -1,14 +1,17 @@
 //desenvolva uma função que receba 2 paramentros, o primeiro um elemento para repetir e o segundo p número de vezes que vai repetir.
 //garanta que o indicador para repetir seja um número e que não seja negativo.
 
-let isnumber = number => typeof(number) === 'number' ?  number : 'Você não informou um número valido';
+let isnumber = number => typeof(number) === 'number' ?  number : false; // vou transformar aqui ou me retorna um booleano ou o numero
+
+let verica = num => num === false ? console.log('Voce não informou um numero') : num;
 
 let elementRepeat = (element, numero) => {
     let number = isnumber(numero);
-
+    verica(number);
     for(let i = 1; i <= number; i++){
         console.log(element + i);
     }
 }
 
-elementRepeat('Alexandre', 8);
+elementRepeat('Alexandre', 7); // caso o valor for um numero vai me reotrnar as repetições
+elementRepeat('Beatriz', 'batata não é uma fruta'); // vai me retornar a advertencia
